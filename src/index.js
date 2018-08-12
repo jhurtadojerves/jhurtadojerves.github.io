f55dd75cbb34bd85895a090c1247e30e9752757b
Julio Hurtado <juliohurtado.backend@gmail.com>
2018-08-11 20:33:53 -0500

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './containers/Home';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Home />, document.getElementById('container'));
-registerServiceWorker();
